a517b3399aa09163c4ec760214367d2c57fba03c
OhioKid418 <ohiokid418@gmail.com>
2026-07-24 15:39:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -417,3 +417,26 @@ function showTrophyRoom(){
 // -------------------------
 
 showBracket();
+
+function showVoting() {
+    document.getElementById("bracketSection").style.display = "none";
+    document.getElementById("contentSection").style.display = "block";
+
+    document.getElementById("dynamicContent").innerHTML = `
+        <h2>🗳️ Community Voting</h2>
+
+        <div class="strawpoll-embed" style="height:672px; max-width:640px; width:100%; margin:auto; display:flex; flex-direction:column;">
+            <iframe 
+                title="StrawPoll Embed"
+                src="https://strawpoll.com/embed/mpnb1WWAEy5"
+                style="width:100%; flex-grow:1;"
+                frameborder="0"
+                allowfullscreen
+                allowtransparency>
+                Loading...
+            </iframe>
+
+            <script async src="https://cdn.strawpoll.com/dist/widgets.js"></script>
+        </div>
+    `;
+}
